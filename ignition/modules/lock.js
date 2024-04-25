f9099aac307a8ed258877c572a2f9f37f952df37
chris anatalio <chris@spheron.network>
2024-04-25 15:44:53 -0700

    Update contract

---
+++ ignition/modules/lock.js
@@ -0,0 +1,12 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+module.exports = buildModule("Lock", (m) => {
+
+    const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+
+    const unlockTime = currentTimestampInSeconds + 60;
+
+    const lock = m.contract("Lock", [unlockTime]);
+
+    return lock;
+});
